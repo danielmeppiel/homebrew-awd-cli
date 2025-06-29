ac219e4a3714aa5b4c50d57e06229d0267b196ea
Daniel Meppiel <51440732+danielmeppiel@users.noreply.github.com>
2025-06-30 00:24:55 +0200

    fix: align all SHA placeholders with workflow expectations
    - ARM64: Use PLACEHOLDER_ARM64_SHA instead of hardcoded SHA
    - x86_64: Use PLACEHOLDER_X86_64_SHA instead of PLACEHOLDER_AMD64_SHA  
    - Linux: Use PLACEHOLDER_LINUX_SHA instead of PLACEHOLDER_LINUX_AMD64_SHA
    
    This ensures the workflow can properly replace all checksums on future releases.

--- Formula/awd-cli.rb
+++ Formula/awd-cli.rb
@@ -7,16 +7,16 @@ class AwdCli < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/danielmeppiel/awd-cli/releases/download/v#{version}/awd-darwin-arm64"
-      sha256 "ca42e25f7056e502d5aaf9775334824e376cc7cf600ddbf32a4818a385fc3658"  # This will be updated automatically
+      sha256 "PLACEHOLDER_ARM64_SHA"  # This will be updated automatically
     else
       url "https://github.com/danielmeppiel/awd-cli/releases/download/v#{version}/awd-darwin-x86_64"
-      sha256 "PLACEHOLDER_AMD64_SHA"  # This will be updated automatically
+      sha256 "PLACEHOLDER_X86_64_SHA"  # This will be updated automatically
     end
   end
 
   on_linux do
     url "https://github.com/danielmeppiel/awd-cli/releases/download/v#{version}/awd-linux-x86_64"
-    sha256 "PLACEHOLDER_LINUX_AMD64_SHA"  # This will be updated automatically
+    sha256 "PLACEHOLDER_LINUX_SHA"  # This will be updated automatically
   end
 
   def install
