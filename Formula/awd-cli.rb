c59e81d50fdb11ba077a6c2ce462129a9fc45e95
GitHub Action <action@github.com>
2025-06-30 10:12:51 +0000

    Update AWD CLI to vv0.0.8

--- Formula/awd-cli.rb
+++ Formula/awd-cli.rb
@@ -6,13 +6,13 @@ class AwdCli < Formula
 
   if Hardware::CPU.arm? && OS.mac?
     url "https://github.com/danielmeppiel/awd-cli/releases/download/v#{version}/awd-darwin-arm64.tar.gz"
-    sha256 "PLACEHOLDER_DARWIN_ARM64_SHA_FOR_TAR_GZ"
+    sha256 ""
   elsif Hardware::CPU.intel? && OS.mac?
     url "https://github.com/danielmeppiel/awd-cli/releases/download/v#{version}/awd-darwin-x86_64.tar.gz"
-    sha256 "PLACEHOLDER_DARWIN_X86_64_SHA_FOR_TAR_GZ"
+    sha256 ""
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/danielmeppiel/awd-cli/releases/download/v#{version}/awd-linux-x86_64.tar.gz"
-    sha256 "PLACEHOLDER_LINUX_X86_64_SHA_FOR_TAR_GZ"
+    sha256 ""
   end
 
   def install
