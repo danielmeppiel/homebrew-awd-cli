bd5a47a193ba66746131b8cb415da8ccb2d7cb27
Daniel Meppiel <51440732+danielmeppiel@users.noreply.github.com>
2025-06-30 12:20:44 +0200

    Fix SHA256 checksums for v0.0.8 release

--- Formula/awd-cli.rb
+++ Formula/awd-cli.rb
@@ -6,13 +6,13 @@ class AwdCli < Formula
 
   if Hardware::CPU.arm? && OS.mac?
     url "https://github.com/danielmeppiel/awd-cli/releases/download/v#{version}/awd-darwin-arm64.tar.gz"
-    sha256 ""
+    sha256 "88fb1fc437e4edab136946757ae3837d156aeee902f12d7086f078d9f8d11fd3"
   elsif Hardware::CPU.intel? && OS.mac?
     url "https://github.com/danielmeppiel/awd-cli/releases/download/v#{version}/awd-darwin-x86_64.tar.gz"
-    sha256 ""
+    sha256 "5046ed478deb7df7516716ac2e87d11ee0fadd1ed1c2134e9e0154377ea26dc3"
   elsif OS.linux? && Hardware::CPU.intel?
     url "https://github.com/danielmeppiel/awd-cli/releases/download/v#{version}/awd-linux-x86_64.tar.gz"
-    sha256 ""
+    sha256 "52ac12b41b5fb0011d79d7e3720bb5ab9ed41a1dce120d58a41c4ec670b78971"
   end
 
   def install
